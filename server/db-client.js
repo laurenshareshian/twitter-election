002ca57dda2ff3ceafac0ef967d06f8da965f558
laurenshareshian <laurenshareshian@gmail.com>
2018-09-07 16:47:24 -0700

    heroku

--- server/db-client.js
+++ server/db-client.js
@@ -1,7 +1,7 @@
 // As we move to into deployment, this value will not
 // be hard-coded, but instead come from environment configuration
 //const DATABASE_URL = 'postgres://localhost:5432/election';
-const DATABASE_URL = 'postgres://dujbduluhcsqsf:fd8a1593b2c4b79d008f5cc8c8c2226605254c2c418d1e4a0adcab26d7f33d82@ec2-184-72-247-70.compute-1.amazonaws.com:5432/d3igvnbs8abcvm';
+
 const pg = require('pg');
 const Client = pg.Client;
 
