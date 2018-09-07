2893dd2a70047d83f81975595edc81416cce4933
laurenshareshian <laurenshareshian@gmail.com>
2018-09-07 16:41:48 -0700

    updated database file

--- server/db-client.js
+++ server/db-client.js
@@ -1,7 +1,7 @@
 // As we move to into deployment, this value will not
 // be hard-coded, but instead come from environment configuration
 //const DATABASE_URL = 'postgres://localhost:5432/election';
-const DATABASE_URL = 'postgres://yaymmkodtdujxh:bce4b71943d7b6e6ecc45a2ff5ac8d932a97108d96ecce0d117ac2c83dd680ac@ec2-54-221-237-246.compute-1.amazonaws.com:5432/dfi2t4t12hfr2h';
+const DATABASE_URL = 'postgres://dujbduluhcsqsf:fd8a1593b2c4b79d008f5cc8c8c2226605254c2c418d1e4a0adcab26d7f33d82@ec2-184-72-247-70.compute-1.amazonaws.com:5432/d3igvnbs8abcvm';
 const pg = require('pg');
 const Client = pg.Client;
 
